28ac67b79a62a995c0646b98fc470c27a03685e7
Ian Njuguna <ian.njuguna@student.moringaschool.com>
2025-06-27 09:38:56 +0300

    array reduce() lab

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = batteryBatches.reduce((total, batch) =>{
+    return total + batch;//total-this is the accumulator starting at 0, while batch is the current item in the array
+}, 0);//initial value is 0
+console.log(totalBatteries); 
 
-// Code your solution here
+
+
